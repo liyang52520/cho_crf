1075977049075747a6c0bf3c57262821c4b499b9
liyang52520 <378776683@qq.com>
2020-09-23 15:29:25 +0800

    master

--- parser/utils/fn.py
+++ parser/utils/fn.py
@@ -8,9 +8,9 @@ def half_width(token):
 
 
 def pad(tensors, padding_value=0):
-    size = [len(tensors)] + [max(tensor.n_labels(i) for tensor in tensors)
-                             for i in range(len(tensors[0].n_labels()))]
+    size = [len(tensors)] + [max(tensor.size(i) for tensor in tensors)
+                             for i in range(len(tensors[0].size()))]
     out_tensor = tensors[0].data.new(*size).fill_(padding_value)
     for i, tensor in enumerate(tensors):
-        out_tensor[i][[slice(0, i) for i in tensor.n_labels()]] = tensor
+        out_tensor[i][[slice(0, i) for i in tensor.size()]] = tensor
     return out_tensor
