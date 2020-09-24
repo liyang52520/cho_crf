5ace27e5b5d6877e79c03046b2624882dde8da5e
liyang52520 <378776683@qq.com>
2020-09-24 14:49:02 +0800

    master

---
+++ train_ctb5.1.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+nohup python -u run.py train --label-ngram 1 --preprocess --device 0  --dir exp/ctb5.master.1 --ftrain data/pos/ctb5/train.conll --fdev data/pos/ctb5/dev.conll --ftest data/pos/ctb5/test.conll > results/ctb5.master.1 2>&1 &
--- train_ctb6.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-nohup python -u run.py train --label-ngram 2 --label-smoothing --preprocess --device 7 --dir exp/ctb6.char --ftrain data/pos/ctb6/train.conll --fdev data/pos/ctb6/dev.conll --ftest data/pos/ctb6/test.conll > results/ctb6.char 2>&1 &
