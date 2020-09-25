6167223adfdbbb202dae439bb8a4bc037cf006ba
liyang52520 <378776683@qq.com>
2020-09-25 12:23:02 +0800

    restore

--- train_ctb5.sh
+++ train_ctb5.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-nohup python -u run.py train --preprocess --device 1  --dir exp/ctb5.minus_span --ftrain data/pos/ctb5/train.conll --fdev data/pos/ctb5/dev.conll --ftest data/pos/ctb5/test.conll > results/ctb5.minus_span 2>&1 &
+nohup python -u run.py train --preprocess --embedding --device 1  --dir exp/ctb5.minus_span --ftrain data/pos/ctb5/train.conll --fdev data/pos/ctb5/dev.conll --ftest data/pos/ctb5/test.conll > results/ctb5.minus_span 2>&1 &
