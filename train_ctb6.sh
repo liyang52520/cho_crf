95d94b619f945a9f63de58cf8d3360509498743f
liyang52520 <378776683@qq.com>
2020-09-25 12:30:57 +0800

    sh

--- train_ctb6.sh
+++ train_ctb6.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-nohup python -u run.py train --preprocess --device 7 --dir exp/ctb6.combine --ftrain data/pos/ctb6/train.conll --fdev data/pos/ctb6/dev.conll --ftest data/pos/ctb6/test.conll > results/ctb6.combine 2>&1 &
+nohup python -u run.py train --preprocess --embedding --device 7 --dir exp/ctb6.combine --ftrain data/pos/ctb6/train.conll --fdev data/pos/ctb6/dev.conll --ftest data/pos/ctb6/test.conll > results/ctb6.combine 2>&1 &
