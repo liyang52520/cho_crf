cf6754e72b62bfeb81aee85f21c40146d5c51827
liyang52520 <378776683@qq.com>
2020-09-28 19:58:00 +0800

    simplify model.

--- parser/model.py
+++ parser/model.py
@@ -25,15 +25,10 @@ class Model(nn.Module):
         self.embed_dropout = IndependentDropout(p=args.embed_dropout)
 
         # the lstm layer
-        self.lstm_unigram = BiLSTM(input_size=n_lstm_input,
-                                   hidden_size=args.n_lstm_hidden,
-                                   num_layers=args.n_lstm_layers,
-                                   dropout=args.lstm_dropout)
-
-        self.lstm_bigram = BiLSTM(input_size=n_lstm_input,
-                                  hidden_size=args.n_lstm_hidden,
-                                  num_layers=args.n_lstm_layers,
-                                  dropout=args.lstm_dropout)
+        self.lstm = BiLSTM(input_size=n_lstm_input,
+                           hidden_size=args.n_lstm_hidden,
+                           num_layers=args.n_lstm_layers,
+                           dropout=args.lstm_dropout)
         self.lstm_dropout = SharedDropout(p=args.lstm_dropout)
 
         # the MLP layers
@@ -107,29 +102,20 @@ class Model(nn.Module):
             embed = self.embed_dropout(word_embed)[0]
 
         # lstm
-        # unigram去掉<bos>再输入到lstm中
-        x_1 = pack_padded_sequence(embed[:, 1:], lens - 1, True, False)
-        x_1, _ = self.lstm_unigram(x_1)
-        x_1, _ = pad_packed_sequence(x_1, True, total_length=seq_len - 1)
-        # x_1: [batch_size, seq_len - 1, n_lstm_hidden * 2]
-        x_1 = self.lstm_dropout(x_1)
-        # bigram
-        x_2 = pack_padded_sequence(embed, lens, True, False)
-        x_2, _ = self.lstm_bigram(x_2)
-        x_2, _ = pad_packed_sequence(x_2, True, total_length=seq_len)
-        x_2 = self.lstm_dropout(x_2)
-        # x_2: [batch_size, seq_len - 1, n_lstm_hidden * 4]
-        x_2 = torch.cat((x_2[:, :-1], x_2[:, 1:]), -1)
+        x = pack_padded_sequence(embed, lens, True, False)
+        x, _ = self.lstm(x)
+        x, _ = pad_packed_sequence(x, True, total_length=seq_len)
+        x = self.lstm_dropout(x)
 
         # mlp
         # emits_1: [batch_size, seq_len - 1, n_labels]
-        emits_1 = self.mlp_unigram(x_1)
-        # emits_2: [batch_size, seq_len - 1, n_labels ** 2]
-        emits_2 = self.mlp_bigram(x_2)
-        # emits_2: [batch_size, seq_len - 1, n_labels, n_labels]
-        emits_2 = emits_2.view(*emits_1.shape, -1)
+        emits_1 = self.mlp_unigram(x[:, 1:])
 
-        # seq_len - 1才是原句子长度（即不包含<bos>或<eos>）
+        # x: [batch_size, seq_len - 1, n_lstm_hidden * 4]
+        x = torch.cat((x[:, :-1], x[:, 1:]), -1)
+        # emits_2: [batch_size, seq_len - 1, n_labels, n_labels]
+        emits_2 = self.mlp_bigram(x).view(*emits_1.shape, -1)
+
         # emits: [batch_size, seq_len - 1, 1, n_labels] + [batch_size, seq_len - 1, n_labels, n_labels]
         #       => [batch_size, seq_len - 1, n_labels, n_labels]
         emits = emits_1.unsqueeze(2) + emits_2
