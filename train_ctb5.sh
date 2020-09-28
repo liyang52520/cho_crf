77a86aca0cf39811d0ad431bbb4ece84f68933ec
liyang52520 <378776683@qq.com>
2020-09-28 16:50:52 +0800

    sh

--- train_ctb5.sh
+++ train_ctb5.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-nohup python -u run.py train --preprocess --embedding --device 3  --dir exp/ctb5.improve_3 --ftrain data/pos/ctb5/train.conll --fdev data/pos/ctb5/dev.conll --ftest data/pos/ctb5/test.conll > results/ctb5.improve_3 2>&1 &
+nohup python -u run.py train --preprocess --embedding --device 0  --dir exp/ctb5.improve_3 --ftrain data/pos/ctb5/train.conll --fdev data/pos/ctb5/dev.conll --ftest data/pos/ctb5/test.conll > results/ctb5.improve_3 2>&1 &
--- train_ctb6.sh
+++ train_ctb6.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-nohup python -u run.py train --preprocess --embedding --device 4 --dir exp/ctb6.improve_3 --ftrain data/pos/ctb6/train.conll --fdev data/pos/ctb6/dev.conll --ftest data/pos/ctb6/test.conll > results/ctb6.improve_3 2>&1 &
+nohup python -u run.py train --preprocess --embedding --device 1 --dir exp/ctb6.improve_3 --ftrain data/pos/ctb6/train.conll --fdev data/pos/ctb6/dev.conll --ftest data/pos/ctb6/test.conll > results/ctb6.improve_3 2>&1 &
